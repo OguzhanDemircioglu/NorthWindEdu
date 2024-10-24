df0a026e8cfdf72a305f3a2909c837b7c93b7133
OguzhanMonster <oguzhanturgut611@gmail.com>
2024-10-24 15:49:51 +0300

    ethereum fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import AdminConsole from "./views/AdminConsole";
@@ -13,7 +13,26 @@ function App() {
     const currentUser = useSelector((state) => state.user);
     const isLoggedIn = currentUser?.token;
 
-    return (
+  useEffect(() => {
+    const loadEthereum = async () => {
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+          console.log('MetaMask is connected');
+        } catch (error) {
+          console.error('User denied account access', error);
+        }
+      } else {
+        console.error('Ethereum provider (MetaMask) not found');
+      }
+    };
+
+    window.addEventListener('load', loadEthereum);
+
+    return () => window.removeEventListener('load', loadEthereum);
+  }, []);
+  
+  return (
         <div className="App">
             {isLoggedIn && <HorizontalMenu/>}
             <Routes>
