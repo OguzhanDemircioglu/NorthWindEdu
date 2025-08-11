195fa00b6b0f1ab5672b339bc3618266ffb327dd
Barkan Çek <barkancek2000@gmail.com>
2025-08-11 14:50:55 +0300

    Add Customer Controller

---
+++ server/src/main/java/com/server/app/controller/CustomerController.java
@@ -0,0 +1,50 @@
+package com.server.app.controller;
+
+import com.server.app.model.Customer;
+import com.server.app.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer){
+        Customer newCustomer = customerService.saveCustomer(customer);
+        return ResponseEntity.ok(newCustomer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable String id, @RequestBody Customer customer){
+        customer.setCustomerId(id);
+        Customer updatedCustomer = customerService.saveCustomer(customer);
+        return ResponseEntity.ok(updatedCustomer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable String id){
+        customerService.deleteCustomerByCustomerId(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable String id){
+        Customer customer = customerService.findCustomerByCustomerId(id);
+        return ResponseEntity.ok(customer);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Customer> getCustomerByName(@RequestParam String name){
+        Optional<Customer> customer = customerService.findCustomerByContactName(name);
+        return customer.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
